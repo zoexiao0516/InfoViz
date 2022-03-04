197d8d1a55d5600810cae238bcc1dcf4d6ef9fbf
Zoe Yajie Xiao <yx1750@nyu.edu>
2022-03-04 15:24:53 +0400

    Q2.5 Copy

---
+++ Assignment2/Q2.6/scales.js
@@ -0,0 +1,15 @@
+export const Scales = {
+    linear: (min_value, max_value, start_pos, end_pos) => {
+        console.log('the linear scale for scatter plot/bar chart');
+        return d3.scaleLinear()
+            .domain([min_value, max_value])
+            .range([start_pos, end_pos])
+            .nice();
+    },
+    band: (stations, start_pos, end_pos) => {
+        console.log('the x scale for the bar chart');
+        return d3.scaleBand()
+            .domain(stations)
+            .range([start_pos, end_pos]);
+    }
+}
