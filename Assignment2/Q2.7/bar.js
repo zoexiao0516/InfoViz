d2185403d12b88795b628d87aaacc000fc2b6ac3
Zoe Yajie Xiao <yx1750@nyu.edu>
2022-03-05 17:18:05 +0400

    Q2.6 Copy

---
+++ Assignment2/Q2.7/bar.js
@@ -0,0 +1,59 @@
+export let drawBars = (barChatLayer, data, xScale, yScale, barChartWidth, barChartHeight, div) => {
+    console.log("This function draws bars in the bar chart.");
+
+    // ---------- draw x & y axis for the bar chart ----------
+    let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale).ticks(5);
+
+    barChatLayer.append('g')
+        .attr('class', 'x-axis')
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", function (d) {
+            return "rotate(-65)"
+        });
+
+    barChatLayer.append('g')
+        .attr("transform", "translate(0," + (-barChartHeight / 2 + 70) + ")")
+        .attr('class', 'y-axis')
+        .call(yAxis);
+
+    // ---------- put y axis label ----------
+    barChatLayer.append('g')
+        .attr("class", 'axis-label')
+        .attr('transform', 'translate(40,' + (-barChartHeight / 2 + 60) + ')')
+        .append("text")
+        .style("text-anchor", 'middle')
+        .text("Bikers start from")
+
+    // ---------- draw bars ----------
+    barChatLayer.selectAll('.bar')
+        .data(data).enter()
+        .append('rect')
+        .attr('class', "bar")
+        .attr('id', d => d.id + "bar")
+        .attr('x', d => xScale(`${d.station}`))
+        .attr('y', d => yScale(d.start))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => { return barChartHeight / 1.5 - yScale(d.start) - 170 })
+        .attr("transform", "translate(0," + (-barChartHeight / 2 + 70) + ")")
+        .style('fill', 'steelblue')
+        .style('stroke', 'black')
+        .style('stroke-width', '2')
+        ;
+
+    // ---------- mouseover events ----------
+    barChatLayer.selectAll(".bar")
+        .on("mouseover", function (event, d) {
+            d3.select('#' + d.id + "bar").transition().style("fill", "red");
+            d3.selectAll('#' + d.id + 'point').MoveToFront();
+            d3.select('#' + d.id + "point").transition().attr("r", 10).style("fill", "red");
+        })
+        .on("mouseout", function (event, d) {
+            d3.select('#' + d.id + "bar").transition().style("fill", "steelblue");
+            d3.select('#' + d.id + "point").transition().attr('r', '5').style("fill", "steelblue");
+        });
+}
---
+++ Assignment2/Q2.7/main.js
@@ -0,0 +1,82 @@
+import { Scales } from './scales.js';
+import { getDataByMonth } from './data_filter.js';
+import { drawPoints } from './scatter.js';
+import { drawBars } from './bar.js';
+
+const svg = d3.select('svg');
+const WIDTH = svg.attr('width');
+const HEIGHT = svg.attr('height');
+let div = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+d3.csv("citi_bike_2020.csv").then(function (data) {
+  // some framework-codes are given to guide you complete the tasks;
+  // in some places, you need to modify them so that they will adapt to your code.
+  // Q2.1 Scatter Plot
+  // ---------- converting the attributes into numbers ----------
+  data.forEach(element => {
+    element.latitude = +element.latitude
+    element.longitude = +element.longitude
+    element.start = +element.start
+    element.tripdurationS = +element.tripdurationS
+    element.end = +element.end
+    element.tripdurationE = +element.tripdurationE
+    element.id = element.month + element.station.replaceAll(' ', '').replaceAll('&', '').replaceAll('-', '')
+  });
+
+  // ---------- set the boundary of the canvas ----------
+  const margin = { left: 300, right: 0, top: 300, bottom: 0, gap: 70 };
+  const width = WIDTH - margin.left;
+  const height = HEIGHT - margin.top;
+
+  // ---------- set the scales for scatter plot ----------
+  const xScale_spl = Scales.linear(0, d3.max(data, d => d.tripdurationS), 0, width);
+  const yScale_spl = Scales.linear(0, d3.max(data, d => d.tripdurationE), (height / 2 - margin.gap), 0);
+
+  let scatterPlotLayer = svg.append("g")
+    .attr("transform", "translate(" + 70 + "," + 70 + ")");
+
+
+  drawPoints(scatterPlotLayer, getDataByMonth(data, 'May'), xScale_spl, yScale_spl, div, width, height);
+
+  // Q2.2 Bar Chart
+  // ---------- sort the data for x scale and bars (May) ----------
+  let sortedMayData = getDataByMonth(data, "May").sort(function (a, b) {
+    return d3.descending(a.start, b.start)
+  })
+
+  // ---------- set the scales for bar chart ----------
+  const xScale_bar = Scales.band(sortedMayData.map(d => `${d.station}`), 0, width);
+  const yScale_bar = Scales.linear(0, d3.max(data, d => d.start), (height / 2 - margin.gap), 0);
+
+  let barChartLayer = svg.append("g")
+    .attr("transform", "translate(" + 70 + "," + (height / 2 + margin.top + margin.gap) + ")");
+
+  drawBars(barChartLayer, sortedMayData, xScale_bar, yScale_bar, width, height, div);
+
+  //Q2.4 Slider
+  let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  let slider = d3.select('#slider');
+  let slidertext = d3.select('#slidertext');
+  slider.on("input", function () {
+    console.log(this.value);
+    slidertext.attr('value', month[this.value - 1]);
+    d3.selectAll('.point').remove();
+    d3.selectAll('.bar').remove();
+    d3.selectAll('.x-axis').remove();
+    d3.selectAll('.y-axis').remove();
+    d3.selectAll('.axis-label').remove();
+
+    // ---------- sort the data for x scale and bars ----------
+    let sortedMonthlyData = getDataByMonth(data, month[this.value - 1]).sort(function (a, b) {
+      return d3.descending(a.start, b.start)
+    })
+
+    const xScale_bar = Scales.band(sortedMonthlyData.map(d => `${d.station}`), 0, width);
+    drawPoints(scatterPlotLayer, getDataByMonth(data, month[this.value - 1]), xScale_spl, yScale_spl, div, width, height);
+    drawBars(barChartLayer, sortedMonthlyData, xScale_bar, yScale_bar, width, height, div);
+  });
+
+
+});
